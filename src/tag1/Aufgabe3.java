8167954fd6a3f65186a4d12ee38e9be25fb6e874
64k <64k>
2015-04-13 15:19:16 +0200

    Aufgabe 3

---
+++ src/tag1/Aufgabe3.java
@@ -0,0 +1,40 @@
+package tag1;
+
+public class Aufgabe3 {
+
+	public int las(int[] a) {
+		int[][] alternation = new int[a.length][2];
+		for (int i = 0; i < a.length; i++) {
+
+			alternation[i][0] = 1;
+			alternation[i][1] = 1;
+
+			for (int j = 0; j < i - 1; j++) {
+
+				if (a[i] - a[j] > 0) {
+					alternation[i][0] = Math.max(alternation[j][1] + 1,
+							alternation[i][0]);
+				} else {
+					if (a[i] - a[j] < 0) {
+						alternation[i][1] = Math.max(alternation[j][0] + 1,
+								alternation[i][1]);
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < alternation.length; i++) {
+			// System.out.println(alternation[i][1]);
+
+		}
+		return Math.max(alternation[a.length - 1][0],
+				alternation[a.length - 1][0]);
+	}
+
+	public static void main(String[] args) {
+		int[] array = { 1,2,-10};
+
+		long result = (new Aufgabe3()).las(array);
+		System.out.println(result);
+	}
+}
