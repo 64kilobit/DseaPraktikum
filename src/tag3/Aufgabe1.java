ff231e899542205d5c36d4b653f45bfaef39cb7c
64k <64k>
2015-04-15 10:40:10 +0200

    Aufgabe 1

--- src/tag3/Aufgabe1.java
+++ src/tag3/Aufgabe1.java
@@ -1,10 +1,61 @@
 package tag3;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Aufgabe1 {
 
-	
-	
+	private static Map<String, Integer> cache = new HashMap<String, Integer>();
+
+	/**
+	 * Gets the min number of operations
+	 * 
+	 * @param string
+	 * @return
+	 */
+	public static int findMiniPalindrome(String string) {
+
+		// debug, print tested substring
+		// System.out.println(string);
+
+		// lookup cache
+		if (cache.containsKey(string)) {
+			return cache.get(string);
+		}
+
+		if (string.length() == 0)
+			return 0;
+		if (string.length() == 1)
+			return 1;
+
+		//replace same consecutive characters with one aaaaaaabbbbbbb => ab
+		String stringShort = string.replaceAll("(.)\\1+", "$1");
+		int min = Integer.MAX_VALUE;
+		
+		for (int i = 0; i < stringShort.length(); i++) {
+
+			// delete one character
+			String tempString = stringShort.substring(0, i)
+					+ stringShort.substring(i + 1, stringShort.length());
+			int tempCount = findMiniPalindrome(tempString) + 1;
+			// get the minimum
+			if (tempCount < min) {
+				min = tempCount;
+			}
+		}
+
+		// cache the result
+		cache.put(string, min);
+
+		return min;
+	}
+
 	public static void main(String[] args) {
+		//tested with strings of length 24
+		String testString = "ABACDBABDFDFJDFDFBCCDFIH";
+
+		int result = findMiniPalindrome(testString);
+		System.out.println(result);
 
 	}
 
